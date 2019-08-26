ff0feadbf0fd86a023c2a6a6796fc6300c6005e6
Alexey Kurpachyov <kurp96@ya.ru>
2019-08-26 16:50:22 +0300

    Реадаптация подвала

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+  // активация адаптивного меню
   $('.header-adaptive__btn').on('click', function(e){
     e.preventDefault();
     $(this).toggleClass('header-adaptive__btn_active');
   });
+  // Раскрытие адаптивного меню
   $('.header-adaptive__btn').on('click', function(e){
     e.preventDefault();
     $('.header-adaptive__menu').toggleClass('header-adaptive__menu_active');
+  });
+  // Высчитывание пропорциональной высоты блока анонсов для соц.сетей
+  $('.anouncement__socials').top($('anouncement-block').height()/1.57);
+  $(window).resize(function(){
+    $('.anouncement__socials').top($('.anouncement-block').height()/1.57);
   });
 });
